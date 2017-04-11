db6e8e8360598988ca6f16e45defec4917d5b3e2
panforrest <guoqianp@gmail.com>
2017-04-11 14:16:38 -0400

    end nov29part2 from homepage click a profile hyperlink to /profil/:username that returns profile._id

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -32399,7 +32399,7 @@ exports.default = ProfileInfo;
 
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -32407,6 +32407,8 @@ var _createClass = function () { function defineProperties(target, props) { for 
 var _react = __webpack_require__(8);
 
 var _react2 = _interopRequireDefault(_react);
+
+var _utils = __webpack_require__(35);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -32417,30 +32419,62 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var Profile = function (_Component) {
-    _inherits(Profile, _Component);
+  _inherits(Profile, _Component);
 
-    function Profile() {
-        _classCallCheck(this, Profile);
+  function Profile() {
+    _classCallCheck(this, Profile);
 
-        return _possibleConstructorReturn(this, (Profile.__proto__ || Object.getPrototypeOf(Profile)).apply(this, arguments));
-    }
+    var _this = _possibleConstructorReturn(this, (Profile.__proto__ || Object.getPrototypeOf(Profile)).call(this));
 
-    _createClass(Profile, [{
-        key: 'render',
-        value: function render() {
-            return _react2.default.createElement(
-                'div',
-                null,
-                _react2.default.createElement(
-                    'h3',
-                    null,
-                    this.props.username
-                )
-            );
+    _this.state = {
+      profile: null
+    };
+    return _this;
+  }
+
+  _createClass(Profile, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      console.log('componentDidMount: ' + this.props.username);
+      _utils.APIManager.get('/api/profile', { username: this.props.username }, function (err, response) {
+        if (err) {
+          alert(err.message);
+          return;
         }
-    }]);
+        console.log(JSON.stringify(response.results[0]));
+        if (response.results.length == 0) {
+          alert('Profile Not Found.');
+          return;
+        }
 
-    return Profile;
+        var profile = response.results[0];
+        _this2.setState({
+          profile: profile
+        });
+
+        // this.props.currentUserReceived(response.results)
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var header = this.state.profile == null ? null : _react2.default.createElement(
+        'h3',
+        null,
+        this.state.profile._id
+      );
+
+      return _react2.default.createElement(
+        'div',
+        null,
+        header
+      );
+    }
+  }]);
+
+  return Profile;
 }(_react.Component);
 
 exports.default = Profile;
--- src/components/containers/Profile.js
+++ src/components/containers/Profile.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+import { APIManager } from '../../utils'
 
 class Profile extends Component {
+	constructor(){
+		super()
+		this.state = {
+			profile: null
+		}
+	}
+
+    componentDidMount(){
+    	console.log('componentDidMount: '+this.props.username)
+    	APIManager.get('/api/profile', {username: this.props.username}, (err, response) => {
+    		if (err) {
+    			alert(err.message)
+    			return
+    		}
+    		console.log(JSON.stringify(response.results[0]))
+            if (response.results.length == 0){
+            	alert('Profile Not Found.')
+            	return
+            }
+
+    		const profile = response.results[0]
+    		this.setState({
+    			profile: profile
+    		})
+
+    		// this.props.currentUserReceived(response.results)
+    	})
+    }
+
     render(){
+        const header = (this.state.profile == null) ? null : <h3>{this.state.profile._id}</h3>
+
     	return(
     		<div>
-    		    <h3>{this.props.username}</h3>
+    		    { header }
     		</div>
     	)
     }
