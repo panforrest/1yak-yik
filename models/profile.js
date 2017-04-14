8dc800c9585bfd729629fa8318fa00a953a16a21
panforrest <guoqianp@gmail.com>
2017-04-14 09:37:14 -0400

    01:23:00 profile add image field; ready for heroku

--- models/profile.js
+++ models/profile.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose')
 var ProfileSchema = new mongoose.Schema({
 	username:{type:String, default:''},
 	city:{type:String, default:''},
+	image:{type:String, default:''},
 	gender:{type:String, default:''},
 	password:{type:String, default:''},
 	timestamp:{type:Date, default:Date.now}  //timestamp:{type:String, default:Date.now}
