debd8fe6ce93d9e06c232069b69d174c178abb68
panforrest <guoqianp@gmail.com>
2017-04-16 16:17:58 -0400

    1:10 index.js matchRoutes function console log success block

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var Promise = require('bluebird')
 
-var app = require('../public/build/es5/serverapp')
+var React = require('react')
+var ReactRouter = require('react-router')
+var ReactDOMServer = require('react-dom/server')
 
-/* GET home page. */
+var serverapp = require('../public/build/es5/serverapp')
+var Home = require('../public/build/es5/components/layout/Home')
+
+matchRoutes = function(req, routes){
+	return new Promise(function(resolve, reject){
+		ReactRouter.match({ routes, location: req.url }, function(error, redirectLoactoin, renderProps){
+			if (error) {
+				reject(error)
+				return
+			}
+
+			resolve(renderProps)
+		})
+	})
+}
+
 router.get('/', function(req, res, next) {
 
-    var testHtml = '<h1>Test Title</h1>'
-    res.render('index', { react:testHtml });
+	var routes = {
+		path: '/',
+		component: serverapp,
+	    // initial: initialStore,
+		indexRoute: {
+			component: Home
+		}
+	}
+
+	matchRoutes(req, routes)
+	.then(function(renderProps){
+		// var html = ReactDOMServer.renderToString(React.createElement(ReactRouter.RouterContext, renderProps))
+        console.log('TEST 1')  //console.log('TEST 1'+html)
+	})
+	.catch(function(err){
+        console.log('TEST 2: '+err)
+	})
+
+    res.render('index', { react: ''});
   // res.render('createzone', { title: 'Express' });
 });
 
