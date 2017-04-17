0dafbfbe4b5f57a54dd6e3fd8fd4370f88fbcac6
panforrest <guoqianp@gmail.com>
2017-04-17 16:46:22 -0400

    50:00 server rendering 3000/profile/forrest14 page view source code see forrest14 in html

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -3,6 +3,18 @@ var mongoose = require('mongoose')
 var bcrypt = require('bcryptjs')
 
 module.exports = {
+	get: function(params){
+		return new Promise(function(resolve, reject){
+		    Profile.find(params, function(err, profiles){
+	        	if(err){
+	        		reject(err)
+	        		return
+	        	}
+	        	resolve(profiles)
+	        })
+		})
+	},
+
 	find: function(params, callback){
         Profile.find(params, function(err, profiles){
         	if(err){
--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,19 @@ var React = require('react')
 var ReactRouter = require('react-router')
 var ReactDOMServer = require('react-dom/server')
 
-var serverapp = require('../public/build/es5/serverapp')
-var store = require('../public/build/es5/stores/store')
-var Home = require('../public/build/es5/components/layout/Home')
+var __BUILD_DIR__ = '../public/build'
+var serverapp = require(__BUILD_DIR__+'/es5/serverapp')
+var store = require(__BUILD_DIR__+'/es5/stores/store')
+var Home = require(__BUILD_DIR__+'/es5/components/layout/Home')
+var ProfileInfo = require(__BUILD_DIR__+'/es5/components/layout/ProfileInfo')
+
+// var serverapp = require('../public/build/es5/serverapp')
+// var store = require('../public/build/es5/stores/store')
+// var Home = require('../public/build/es5/components/layout/Home')
 
 matchRoutes = function(req, routes){
 	return new Promise(function(resolve, reject){
-		ReactRouter.match({ routes, location: req.url }, function(error, redirectLoactoin, renderProps){
+		ReactRouter.match({ routes: routes, location: req.url }, function(error, redirectLoactoin, renderProps){
 			if (error) {
 				reject(error)
 				return
@@ -101,16 +107,63 @@ router.get('/:page/:slug', function(req, res, next){
 	var page = req.params.page
 	var slug = req.params.slug
 
+	var initialStore = null
+	var reducers = {}
+
 	if (page == 'api'){
 		next()
 		return
 	}
 
+	if (page == 'account'){
+		next()
+		return
+	}
+
+	controllers.profile.get({username: slug})
+	.then(function(profiles){
+        var profile = profiles[0]
+        var profileMap = {}
+        profileMap[slug] = profile
+
+        reducers['profile'] = {
+	        list: [profile],   //list: profile,
+	        map: profileMap,
+	        appStatus: 'ready'
+	    }
+
+	    initialStore = store.configureStore(reducers)
+
+	    var reoutes = {
+			path: '/profile/:username',
+			component: serverapp,
+		    initial: initialStore,
+			indexRoute: {
+				component: ProfileInfo   //Home
+			}
+	    }
+
+	    return matchRoutes(req, routes)
+	})
+	.then(function(renderProps){
+		var html = ReactDOMServer.renderToString(React.createElement(ReactRouter.RouterContext, renderProps))//TAKE REACT COMPONENT TO HTML
+        res.render('index', { react: html, preloadedState: JSON.stringify(initialStore.getState()) })
+    })
+    .catch(function(err){
+
+    })
 })
 
-router.get('/:page', function(req, res, next) {
-    res.render(req.params.page, null);	
+// router.get('/:page', function(req, res, next) {
+//     res.render(req.params.page, null);	
+// });
 
-});
+router.get('/createzone', function(req, res, next) {
+  res.render('createzone', null)
+})
+
+router.get('/createcomment', function(req, res, next) {
+  res.render('createcomment', null)
+})
 
 module.exports = router;
