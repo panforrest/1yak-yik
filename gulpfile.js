e5cc4788b13175341b26363a131f01d42e2b272e
panforrest <guoqianp@gmail.com>
2017-04-19 15:57:43 -0400

    51:00 combine css files and compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp')
 var to5 = require('gulp-6to5')
+var less = require('gulp-less')
+var gp_concat = require('gulp-concat')
+var minifyCSS = require('gulp-minify-css')
+var autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('es6-es5', function(){
 	return gulp.src([
@@ -12,8 +16,27 @@ gulp.task('es6-es5', function(){
 		.pipe(gulp.dest('./public/build/es5/'))  //STRICTLY BENEFIT THE SERVER
 })
 
+gulp.task('css', function(){
+	return gulp.src(
+		[
+            './public/css/bootstrap.css',
+            './public/css/style.css',
+            './public/css/swiper.css',
+            './public/css/dark.css',
+            './public/css/font-icons.css',
+            './public/css/animate.css',
+            './public/css/magnific-popup.css',
+            './public/css/responsive.css'
+		]
+	)
+	.pipe(minifyCSS())
+	.pipe(autoprefixer('last 2 version', 'sarari 5', 'ie 8', 'ie 9'))
+	.pipe(gp_concat('style.min.css'))
+	.pipe(gulp.dest('./public/build/css/'))
+})
+
 gulp.task('watch', function(){
     gulp.watch(['./src/serverapp.js', './src/*/**.js', './src/*/*/**.js'],['es6-es5'])
 })
 
-gulp.task('default', ['es6-es5', 'watch'], function(){})
+gulp.task('default', ['es6-es5', 'css', 'watch'], function(){})
